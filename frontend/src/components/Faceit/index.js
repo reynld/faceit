afdd56b4834d0e7467d8bd9c549808b79a30e408
Chris Cedeno <chrisrey95@gmail.com>
2019-06-18 22:16:56 -0400

    Final ELO

--- frontend/src/components/Faceit/index.js
+++ frontend/src/components/Faceit/index.js
@@ -55,7 +55,7 @@ class Faceit extends Component {
     })
   }
 
-  addUserElo(teamId, userId, elo = 1025) {
+  addUserElo(teamId, userId, elo = 1000) {
     this.setState(prevState => {
       let teamElo = Object.assign({}, prevState.teamElo);
       if (!teamElo[teamId]) {
